2edc5dbda5b25c85eb9647550cd40159b728f45d
ValentinBctt <valentin.bctt@gmail.com>
2026-03-29 19:28:59 +0200

    image render2

--- app/frontend/components/atelier/Selection_textile.jsx
+++ app/frontend/components/atelier/Selection_textile.jsx
@@ -4,7 +4,7 @@ export default function SelectionTextile() {
   return (
     <div className="selection-textile">
         <h2>Notre selection de textile</h2>
-        <p>plop</p>
+      
     </div>
   )
 }
