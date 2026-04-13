9534b0a8938877dde893fb22c3d90291876971be
ValentinBctt <valentin.bctt@gmail.com>
2026-04-13 17:41:51 +0200

    colorpicker fix

--- app/fields/color_picker_multiple_field.rb
+++ app/fields/color_picker_multiple_field.rb
@@ -1,17 +1,13 @@
 # Champ personnalisé Administrate pour sélectionner plusieurs couleurs avec un color picker
 require "administrate/field/base"
 
-module Administrate
-  module Field
-    class ColorPickerMultipleField < Administrate::Field::Base
-      def to_s
-        data.is_a?(Array) ? data.join(", ") : data.to_s
-      end
+class ColorPickerMultipleField < Administrate::Field::Base
+  def to_s
+    data.is_a?(Array) ? data.join(", ") : data.to_s
+  end
 
-      # Forcer l'utilisation du partial custom
-      def to_partial_path
-        "/fields/color_picker_multiple_field/show"
-      end
-    end
+  # Forcer l'utilisation du partial custom
+  def to_partial_path
+    "/fields/color_picker_multiple_field/show"
   end
 end
