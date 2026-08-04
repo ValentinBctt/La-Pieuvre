77086aa284186724ac83c41eb354d947e87bdc7e
ValentinBctt <valentin.bctt@gmail.com>
2026-08-04 11:40:02 +0200

    fix2

--- db/migrate/20260804100000_remove_legacy_image_columns_from_categories.rb
+++ db/migrate/20260804100000_remove_legacy_image_columns_from_categories.rb
@@ -1,4 +1,4 @@
-class RemoveImageColumnsFromCategoriesLate < ActiveRecord::Migration[7.0]
+class RemoveLegacyImageColumnsFromCategories < ActiveRecord::Migration[7.0]
   def change
     return unless table_exists?(:categories)
 
