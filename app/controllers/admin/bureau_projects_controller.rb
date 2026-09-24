97e53ebba1899ddc55501c52b58ab376f1cd3b04
ValentinBctt <valentin.bctt@gmail.com>
2026-09-24 15:31:37 +0200

    fix bug delete image

--- app/controllers/admin/bureau_projects_controller.rb
+++ app/controllers/admin/bureau_projects_controller.rb
@@ -1,16 +1,53 @@
 module Admin
   class BureauProjectsController < Admin::ApplicationController
-    def update
-      if params[:bureau_project] && params[:bureau_project][:remove_photo_ids].present?
-        ids = params[:bureau_project].delete(:remove_photo_ids)
-        project = BureauProject.find(params[:id])
-        project.photos_attachments.where(id: ids).each(&:purge)
-      end
+    def create
+      new_photos = extract_new_photos(params[:bureau_project])
+      resource = new_resource(resource_params)
+      authorize_resource(resource)
 
-      super
+      if resource.save
+        resource.photos.attach(new_photos) if new_photos.present?
+        redirect_to(
+          after_resource_created_path(resource),
+          notice: translate_with_resource("create.success")
+        )
+      else
+        render :new, locals: {
+          page: Administrate::Page::Form.new(dashboard, resource)
+        }, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      project = requested_resource
+      remove_ids = params[:bureau_project]&.delete(:remove_photo_ids)
+      new_photos = extract_new_photos(params[:bureau_project])
+
+      if project.update(resource_params)
+        project.photos_attachments.where(id: remove_ids).each(&:purge) if remove_ids.present?
+        project.photos.attach(new_photos) if new_photos.present?
+
+        redirect_to(
+          after_resource_updated_path(project),
+          notice: translate_with_resource("update.success"),
+          status: :see_other
+        )
+      else
+        render :edit, locals: {
+          page: Administrate::Page::Form.new(dashboard, project)
+        }, status: :unprocessable_entity
+      end
     end
 
     private
+
+    def extract_new_photos(model_params)
+      return [] unless model_params
+
+      Array(model_params.delete(:photos)).compact_blank.reject do |file|
+        file.respond_to?(:original_filename) && file.original_filename.blank?
+      end
+    end
 
     def resource_params
       params.require(:bureau_project).permit(
--- app/controllers/admin/prestation_lives_controller.rb
+++ app/controllers/admin/prestation_lives_controller.rb
@@ -1,16 +1,53 @@
 module Admin
   class PrestationLivesController < Admin::ApplicationController
-    def update
-      if params[:prestation_live] && params[:prestation_live][:remove_photo_ids].present?
-        ids = params[:prestation_live].delete(:remove_photo_ids)
-        prestation = PrestationLive.find(params[:id])
-        prestation.photos_attachments.where(id: ids).each(&:purge)
-      end
+    def create
+      new_photos = extract_new_photos(params[:prestation_live])
+      resource = new_resource(resource_params)
+      authorize_resource(resource)
 
-      super
+      if resource.save
+        resource.photos.attach(new_photos) if new_photos.present?
+        redirect_to(
+          after_resource_created_path(resource),
+          notice: translate_with_resource("create.success")
+        )
+      else
+        render :new, locals: {
+          page: Administrate::Page::Form.new(dashboard, resource)
+        }, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      prestation = requested_resource
+      remove_ids = params[:prestation_live]&.delete(:remove_photo_ids)
+      new_photos = extract_new_photos(params[:prestation_live])
+
+      if prestation.update(resource_params)
+        prestation.photos_attachments.where(id: remove_ids).each(&:purge) if remove_ids.present?
+        prestation.photos.attach(new_photos) if new_photos.present?
+
+        redirect_to(
+          after_resource_updated_path(prestation),
+          notice: translate_with_resource("update.success"),
+          status: :see_other
+        )
+      else
+        render :edit, locals: {
+          page: Administrate::Page::Form.new(dashboard, prestation)
+        }, status: :unprocessable_entity
+      end
     end
 
     private
+
+    def extract_new_photos(model_params)
+      return [] unless model_params
+
+      Array(model_params.delete(:photos)).compact_blank.reject do |file|
+        file.respond_to?(:original_filename) && file.original_filename.blank?
+      end
+    end
 
     def resource_params
       params.require(:prestation_live).permit(
